1e5a5631255d14c3695e8dff5d00a491f9ebd0ca
jetsonhacks <jim@jetsonhacks>
2018-04-26 15:25:49 -0700

    Initial Commit

---
+++ installRealSenseROS.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# Install the Intel Realsense library librealsense 2 on a Jetson TX Development Kit
+# Copyright (c) 2017-2018 Jetsonhacks 
+# MIT License
+# Usage:
+# ./installLibRealSense.sh <catkin workspace>
+# If <catkin workspace> is omitted "catkin_ws" assumed
+# ROS should already be installed, and a catkin workspace created
+# Figure out where to install librealsense
+# Save the directory we're installing from:
+INSTALL_DIR=$PWD
+# Now go get ready to install librealsense
+source /opt/ros/kinetic/setup.bash
+DEFAULTDIR=catkin_ws
+CLDIR="$1"
+if [ ! -z "$CLDIR" ]; then 
+ DEFAULTDIR="$CLDIR"
+fi
+# Check to see if qualified path already
+if [ -d "$DEFAULTDIR" ] ; then
+   echo "Fully qualified path"
+else
+   # Have to add path qualification
+   DEFAULTDIR=$HOME/$DEFAULTDIR
+fi
+echo "DEFAULTDIR: $DEFAULTDIR"
+
+
+
+if [ -e "$DEFAULTDIR" ] ; then
+  echo "$DEFAULTDIR exists" 
+  CATKIN_WORKSPACEHIDDEN=$DEFAULTDIR/.catkin_workspace
+  if [ -e "$CATKIN_WORKSPACEHIDDEN" ] ; then
+	# This appears to be a Catkin_Workspace
+	echo "Found catkin workspace in directory: $DEFAULTDIR"
+  else
+	echo "$DEFAULTDIR does not appear to be a Catkin Workspace"
+        echo "The directory does not contain the hidden file .catkin_workspace"
+	echo "Terminating Installation"
+	exit 1
+  fi
+else 
+  echo "Catkin Workspace named $DEFAULTDIR does not exist"
+  echo "Please create a Catkin Workspace before installation"
+  echo "Terminating Installation"
+  exit 1
+fi
+if [ "${DEFAULTDIR: -1}" != "/" ] ; then
+	DEFAULTDIR=$DEFAULTDIR/
+fi
+
+INSTALLDIR="$DEFAULTDIR"src
+if [ -e "$INSTALLDIR" ] ; then
+  echo "Installing librealsense in: $INSTALLDIR"
+else
+  echo "$INSTALLDIR does not appear to be a source of a Catkin Workspace"
+  echo "The source directory src does not exist"
+  echo "Terminating Installation"
+  exit 1
+fi 
+
+echo "Starting installation of librealsense"
+
+cd $INSTALLDIR
+# The current ROS version of librealsense
+sudo apt-get install ros-kinetic-librealsense -y
+
+cd $INSTALLDIR
+echo "Starting installation of RealSense ROS package"
+
+# Update the dependencies database
+rosdep update
+
+git clone https://github.com/intel-ros/realsense.git
+cd realsense
+git checkout 2.0.3
+cd ../..
+sudo rosdep -y install --from-paths src --ignore-src --rosdistro kinetic
+catkin_make
+echo "RealSense 2 ROS Package installed"
+
